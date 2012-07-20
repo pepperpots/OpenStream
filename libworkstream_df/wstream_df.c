5477a45b9eff86d2c70074f9b36e8936fe795efc
Nhat Minh Le <nhat.minh.le@huoc.org>
2012-07-20 18:36:20 +0200

    Split PAPI definitions in papi-defs.h

--- libworkstream_df/wstream_df.c
+++ libworkstream_df/wstream_df.c
@@ -9,49 +9,7 @@
 #define _WSTREAM_DF_DEBUG 1
 
 //#define _PAPI_PROFILE
-
-#ifdef _PAPI_PROFILE
-#include <papi.h>
-#endif
-
-#define _PAPI_COUNTER_SETS 3
-
-# define _PAPI_P0B
-# define _PAPI_P0E
-# define _PAPI_P1B
-# define _PAPI_P1E
-# define _PAPI_P2B
-# define _PAPI_P2E
-# define _PAPI_P3B
-# define _PAPI_P3E
-
-# define _PAPI_INIT_CTRS(I)
-# define _PAPI_DUMP_CTRS(I)
-
-#ifdef _PAPI_PROFILE
-# define _PAPI_P0B start_papi_counters (0)
-# define _PAPI_P0E stop_papi_counters (0)
-# define _PAPI_P1B start_papi_counters (1)
-# define _PAPI_P1E stop_papi_counters (1)
-# define _PAPI_P2B start_papi_counters (2)
-# define _PAPI_P2E stop_papi_counters (2)
-//# define _PAPI_P3B start_papi_counters (3)
-//# define _PAPI_P3E stop_papi_counters (3)
-
-# define _PAPI_INIT_CTRS(I)			\
-  {						\
-    int _ii;					\
-    for (_ii = 0; _ii < I; ++ _ii)		\
-      init_papi_counters (_ii);			\
-  }
-
-# define _PAPI_DUMP_CTRS(I)			\
-  {						\
-    int _ii;					\
-    for (_ii = 0; _ii < I; ++ _ii)		\
-      dump_papi_counters (_ii);			\
-  }
-#endif
+#include "papi-defs.h"
 
 #include "wstream_df.h"
 #include "error.h"
