a9574026fb915e03a479fb2d339edc586ca507ca
Nhat Minh Le <nhat.minh.le@huoc.org>
2012-08-03 15:04:07 +0200

    Improve iterative tuning in benchmark-cdeque.sh
    Fix initial estimate by dividing by the number of thieves.
    
    Use last frequency as new lower bound for next binary search.

--- libworkstream_df/benchmark-cdeque.sh
+++ libworkstream_df/benchmark-cdeque.sh
@@ -24,32 +24,39 @@ shift $((OPTIND - 1))
 
 tune()
 {
-	awk -v t=$t -v targs="-b $b -d $d -n $nthread" -v ni=$1	\
-	-v expected=$2 -v total=$nsteal '
+	awk -v t=$t -v targs="$testargs"		\
+	    -v n=$nthread -v ni=$1			\
+	    -v hint=$3 -v expected=$2 -v total=$nsteal '
 	BEGIN {
-		a = 0
-		b = 1
-		f = expected / total
+		a = hint
+		b = 1 / (n - 1)
+		f = expected / total / (n - 1)
 		if (expected == 0) {
 			print 0.0
 			exit
 		}
+		if (f < a)
+			a = f
+		est = f
 		for (i = 0; i < ni; ++i) {
 			cmd = t " " targs " -s " expected " -f " f
 			cmd | getline
 			cmd | getline
 			close(cmd)
 			thief_time = 0
-			for (j = 2; j <= NF - 5; ++j)
+			for (j = 2; j <= n; ++j)
 				thief_time += $j
-			thief_time /= NF - 6
+			thief_time /= n - 1
+			printf "%f => %f / %f (%f)\n",
+			    f, thief_time, $1, thief_time / $1 | "cat >&2"
+			est = f
 			if (thief_time < $1)
 				b = f
 			else
 				a = f
 			f = (a + b) / 2
 		}
-		print f
+		print est
 	}
 '
 }
@@ -62,17 +69,21 @@ iter()
 		else
 			farg=
 		fi
-		$t -b $b -d $d -n $nthread -s $2 $farg | tail -n 1
+		$t $testargs -s $2 $farg | tail -n 1
 	done
 }
 
 run()
 {
 	log=$t.$nthread.$b.$d.$1.log
-	echo tuning $t -b $b -d $d -s $1 >&2
-	f=$(tune 10 $1)
-	echo $t -b $b -d $d -s $1 -f $f \> $log >&2
-	[ $dry ] || iter $niter $1 $f >$log
+	echo tuning $t $testargs -s $1 >&2
+	f=$(tune 10 $1 $f)
+	echo $t $testargs -s $1 -f $f \> $log >&2
+	if [ $dry ]; then
+		$t $testargs -s $1 -f $f
+	else
+		iter $niter $1 $f >$log
+	fi
 }
 
 do_tree()
@@ -97,15 +108,18 @@ do_tree()
 	echo jobs $nsteal >&2
 
 	tests='./test-cdeque ./test-cdeque-nofences ./test-cdeque-c11 ./test-cdeque-dumbc11'
+	testargs="-b $b -d $d -n $nthread"
+
 	for t in $tests; do
 		echo $t prerun >&2
 		[ $dry ] || iter 10 0 >/dev/null
+		f=0
 		run 0
 		s=1
 		n10=$((nsteal / 10))
 		while [ $s -lt $n10 ]; do
 			if [ $s -lt $nthread ]; then
-				echo skipping $t -b $b -d $d -s $s >&2
+				echo skipping $t $testargs -s $s >&2
 			else
 				run $s
 			fi
