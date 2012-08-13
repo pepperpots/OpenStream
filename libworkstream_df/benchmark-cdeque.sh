a725e2ac4b9d9737262b80ff416a5b263a03e311
Nhat Minh Le <nhat.minh.le@huoc.org>
2012-08-13 20:05:37 +0200

    Add test selection and randfile specification to benchmark-cdeque.sh

--- libworkstream_df/benchmark-cdeque.sh
+++ libworkstream_df/benchmark-cdeque.sh
@@ -3,7 +3,7 @@ d=15
 mu=$(awk "BEGIN{print ($b ^ $d) / 10}")
 nthread=2
 niter=200
-while getopts b:d:f:n:pt: opt; do
+while getopts b:d:f:n:pr:t: opt; do
 	case $opt in
 	b)
 		b=$OPTARG
@@ -20,32 +20,44 @@ while getopts b:d:f:n:pt: opt; do
 	p)
 		dry=1
 		;;
+	r)
+		randfile=$OPTARG
+		;;
 	t)
 		niter=$OPTARG
 	esac
 done
 shift $((OPTIND - 1))
 
-tests='test-cdeque test-cdeque-nofences test-cdeque-c11 test-cdeque-dumbc11'
+if [ $# -eq 0 ]; then
+	tests='test-cdeque test-cdeque-nofences test-cdeque-c11 test-cdeque-dumbc11'
+else
+	for t; do
+		tests="$tests ${t#./}"
+	done
+fi
 testargs="-b $b -d $d -n $nthread"
 
-awk -v ni=$niter -v mu=$mu '
-BEGIN {
-	for (i = 1; i <= ni; ++i)
-		print -log(rand()) * mu
-}
-' >rand.$$
+if [ -z "$randfile" ]; then
+	randfile=rand.$$
+	awk -v ni=$niter -v mu=$mu '
+	BEGIN {
+		for (i = 1; i <= ni; ++i)
+			print -log(rand()) * mu
+	}
+	' >"$randfile"
 
-t=test-cdeque-nosync
-echo $t prerun >&2
-for i in $(seq 1 10); do
-	[ $dry ] || ./$t $testargs -f 0 | tail -n 1 >/dev/null
-done
-log=$t.$nthread.$b.$d.log
-for i in $(seq 1 30); do
-	echo $t $testargs -f 0 >&2 \>\> $log
-	[ $dry ] || ./$t $testargs -f 0 | tail -n 1 >$log
-done
+	t=test-cdeque-nosync
+	echo $t prerun >&2
+	for i in $(seq 1 10); do
+		[ $dry ] || ./$t $testargs -f 0 | tail -n 1 >/dev/null
+	done
+	log=$t.$nthread.$b.$d.log
+	for i in $(seq 1 30); do
+		echo $t $testargs -f 0 >&2 \>\> $log
+		[ $dry ] || ./$t $testargs -f 0 | tail -n 1 >$log
+	done
+fi
 
 for t in $tests; do
 	echo $t prerun >&2
@@ -57,5 +69,5 @@ for t in $tests; do
 		f=$(awk "BEGIN{print $x / ($nthread-1)}")
 		echo $t $testargs -f $f >&2 \>\> $log
 		[ $dry ] || ./$t $testargs -f $f | tail -n 1 >>$log
-	done <rand.$$
+	done <"$randfile"
 done
