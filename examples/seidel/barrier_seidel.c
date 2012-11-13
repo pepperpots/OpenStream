efed6593f926870041e3b4b9fc6b009b27c80600
Andi Drebes <andi.drebes@lip6.fr>
2012-11-13 13:54:05 +0100

    Seidel: Add parameter checks and documentation

--- examples/seidel/barrier_seidel.c
+++ examples/seidel/barrier_seidel.c
@@ -50,11 +50,10 @@ main (int argc, char **argv)
   int bs = 3;
 
   FILE *res_file = NULL;
-  FILE *in_file = NULL;
 
   int volatile res = 0;
 
-  while ((option = getopt(argc, argv, "n:s:b:r:i:o:")) != -1)
+  while ((option = getopt(argc, argv, "n:s:b:r:o:h")) != -1)
     {
       switch(option)
 	{
@@ -71,14 +70,31 @@ main (int argc, char **argv)
 	case 'r':
 	  numiters = atoi (optarg);
 	  break;
-	case 'i':
-	  in_file = fopen(optarg, "r");
-	  break;
 	case 'o':
 	  res_file = fopen(optarg, "w");
 	  break;
+	case 'h':
+	  printf("Usage: %s [option]...\n\n"
+		 "Options:\n"
+		 "  -n <size>                    Number of colums of the square matrix, default is %d\n"
+		 "  -s <power>                   Set the number of colums of the square matrix to 1 << <power>\n"
+		 "  -b <block size power>        Set the block size 1 << <block size power>, default is %d\n"
+		 "  -r <iterations>              Number of iterations\n"
+		 "  -o <output file>             Write data to output file, default is barrier_seidel.out\n",
+		 argv[0], N, block_size);
+	  exit(0);
+	  break;
+	case '?':
+	  fprintf(stderr, "Run %s -h for usage.\n", argv[0]);
+	  exit(1);
+	  break;
 	}
     }
+
+  if(optind != argc) {
+	  fprintf(stderr, "Too many arguments. Run %s -h for usage.\n", argv[0]);
+	  exit(1);
+  }
 
   if (res_file == NULL)
     res_file = fopen("barrier_seidel.out", "w");
