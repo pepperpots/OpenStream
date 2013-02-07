198c23fe6b0c1c28e418db4a4e658caf7c6ade88
apop <antoniu.pop@gmail.com>
2013-02-07 17:35:15 +0100

    Use array for declaration of cpu_bytes in omp-low.c

--- gcc/gcc/omp-low.c
+++ gcc/gcc/omp-low.c
@@ -51,7 +51,7 @@ typedef struct wstream_df_frame
 {
   tree wstream_df_frame_type;
   tree wstream_df_frame_field_work_fn;
-  tree wstream_df_frame_field_bytes_cpu[MAX_CPUS];
+  tree wstream_df_frame_field_bytes_cpu;
   tree wstream_df_frame_field_last_owner;
   tree wstream_df_frame_field_steal_type;
   tree wstream_df_frame_field_sc;
@@ -7767,20 +7767,16 @@ build_wstream_df_frame_base_type (omp_context *ctx)
      Reversed order to ensure the above order is actually obtained.
   */
 
-  for(i = MAX_CPUS-1; i >=0; --i) {
-	  char tmp[20];
-	  snprintf(tmp, sizeof(tmp), "bytes_cpu%d\n", i);
-	  name = create_tmp_var_name (tmp);
-	  type = integer_type_node;
-	  field = build_decl (gimple_location (ctx->stmt), FIELD_DECL, name, type);
-	  /*  insert_field_into_struct (ctx->record_type, field); */
-	  DECL_CONTEXT (field) = ctx->record_type;
-	  DECL_CHAIN (field) = TYPE_FIELDS (ctx->record_type);
-	  TYPE_FIELDS (ctx->record_type) = field;
-	  if (TYPE_ALIGN (ctx->record_type) < DECL_ALIGN (field))
-		  TYPE_ALIGN (ctx->record_type) = DECL_ALIGN (field);
-	  ctx->base_frame.wstream_df_frame_field_bytes_cpu[i] = field;
-  }
+  name = create_tmp_var_name ("bytes_cpu");
+  type = build_array_type_nelts (integer_type_node, MAX_CPUS);
+  field = build_decl (gimple_location (ctx->stmt), FIELD_DECL, name, type);
+  /*  insert_field_into_struct (ctx->record_type, field); */
+  DECL_CONTEXT (field) = ctx->record_type;
+  DECL_CHAIN (field) = TYPE_FIELDS (ctx->record_type);
+  TYPE_FIELDS (ctx->record_type) = field;
+  if (TYPE_ALIGN (ctx->record_type) < DECL_ALIGN (field))
+    TYPE_ALIGN (ctx->record_type) = DECL_ALIGN (field);
+  ctx->base_frame.wstream_df_frame_field_bytes_cpu = field;
 
   name = create_tmp_var_name ("last_owner");
   type = integer_type_node;
