4af55d8214e34cf0e0cfab1390fdf2fdeca84066
Andi Drebes <andi.drebes@lip6.fr>
2013-02-27 10:51:58 +0100

    Task duration histogram script: add parameter for maximum x-value

--- extras/gen-trace-histogram-sheet.sh
+++ extras/gen-trace-histogram-sheet.sh
@@ -3,12 +3,45 @@
 HISTOGRAM_FILE="task_histogram.gpdata"
 NUM_GRAPHS=`grep '#' $HISTOGRAM_FILE | wc -l`
 
-if [ -z "$1" -o "$1" = "--keep" ]
-then
-    KEEP="true"
-else
-    KEEP="false"
-fi
+KEEP="false"
+MAX_X="*"
+
+check_next_arg()
+{
+    if [ $2 -lt 2 ]
+    then
+	echo "Error: option $1 requires an argument" >&2
+	exit 1
+    fi
+}
+
+check_option_empty()
+{
+    if [ -z "$2" ]
+    then
+	echo "Error: option $1 requires an argument" >&2
+	exit 1
+    fi
+}
+
+while [ $# -gt 0 ]
+do
+    case $1 in
+	--keep|-k)
+	    KEEP="true"
+	    ;;
+	--max-x=*)
+	    OPTION=`echo $1 | cut -d= -f1`
+	    MAX_X=`echo $1 | cut -d= -f2`
+	    check_option_empty "$OPTION" "$MAX_X"
+	    ;;
+	*)
+	    echo "Unknown option $1."
+	    exit 1
+	    ;;
+    esac
+    shift
+done
 
 XLABEL=`awk "NR==1{print;exit}" $HISTOGRAM_FILE | sed 's/#[0-9 ]*: \(.*\)/\1/'`
 
@@ -19,7 +52,7 @@ do
     echo "set title '$TITLE'" ;
     echo "set xlabel '$XLABEL'" ;
     echo "set ylabel ''" ;
-    echo "set xrange [0:*]" ;
+    echo "set xrange [0:$MAX_X]" ;
     echo "set boxwidth 1" ;
     echo "set output \"$i.ps\"" ;
     echo "plot \"$HISTOGRAM_FILE\" using 1:$i with boxes title ''") | gnuplot
@@ -29,7 +62,7 @@ done
 
 pdfjoin --outfile histograms.pdf `seq 2 $NUM_GRAPHS | sed 's/$/.pdf/' | xargs`
 
-if [ $KEEP = "false" ]
+if [ $KEEP != "true" ]
 then
     rm `seq 2 $NUM_GRAPHS | sed 's/$/.pdf/' | xargs`
 fi
