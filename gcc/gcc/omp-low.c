1561babf9499601f50ad25b5c983e9ca2db586f7
Andi Drebes <andi.drebes@lip6.fr>
2013-03-04 16:07:47 +0100

    Correct order of frame fields "creation_timestamp" and "ready_timestamp" in omp-low.c

--- gcc/gcc/omp-low.c
+++ gcc/gcc/omp-low.c
@@ -7770,17 +7770,6 @@ build_wstream_df_frame_base_type (omp_context *ctx)
      Reversed order to ensure the above order is actually obtained.
   */
 
-  name = create_tmp_var_name ("creation_timestamp");
-  type = uint64_type_node;
-  field = build_decl (gimple_location (ctx->stmt), FIELD_DECL, name, type);
-  /* insert_field_into_struct (ctx->record_type, field); */
-  DECL_CONTEXT (field) = ctx->record_type;
-  DECL_CHAIN (field) = TYPE_FIELDS (ctx->record_type);
-  TYPE_FIELDS (ctx->record_type) = field;
-  if (TYPE_ALIGN (ctx->record_type) < DECL_ALIGN (field))
-    TYPE_ALIGN (ctx->record_type) = DECL_ALIGN (field);
-  ctx->base_frame.wstream_df_frame_field_creation_timestamp = field;
-
   name = create_tmp_var_name ("ready_timestamp");
   type = uint64_type_node;
   field = build_decl (gimple_location (ctx->stmt), FIELD_DECL, name, type);
@@ -7791,6 +7780,17 @@ build_wstream_df_frame_base_type (omp_context *ctx)
   if (TYPE_ALIGN (ctx->record_type) < DECL_ALIGN (field))
     TYPE_ALIGN (ctx->record_type) = DECL_ALIGN (field);
   ctx->base_frame.wstream_df_frame_field_ready_timestamp = field;
+
+  name = create_tmp_var_name ("creation_timestamp");
+  type = uint64_type_node;
+  field = build_decl (gimple_location (ctx->stmt), FIELD_DECL, name, type);
+  /* insert_field_into_struct (ctx->record_type, field); */
+  DECL_CONTEXT (field) = ctx->record_type;
+  DECL_CHAIN (field) = TYPE_FIELDS (ctx->record_type);
+  TYPE_FIELDS (ctx->record_type) = field;
+  if (TYPE_ALIGN (ctx->record_type) < DECL_ALIGN (field))
+    TYPE_ALIGN (ctx->record_type) = DECL_ALIGN (field);
+  ctx->base_frame.wstream_df_frame_field_creation_timestamp = field;
 
   name = create_tmp_var_name ("bytes_cpu");
   type = build_array_type_nelts (integer_type_node, MAX_CPUS);
