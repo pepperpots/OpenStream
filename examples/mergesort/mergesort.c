2995b026a12098f2a8b77b504b5ae348b3ca48f6
Andi Drebes <andi.drebes@lip6.fr>
2013-03-12 11:59:33 +0100

    Add mergesort benchmark

---
+++ examples/mergesort/mergesort.c
@@ -0,0 +1,105 @@
+/**
+ * Simple sequential merge sort implementation.
+ *
+ * Copyright (C) 2013 Andi Drebes <andi.drebes@lip6.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <assert.h>
+#include <malloc.h>
+#include <getopt.h>
+#include "mergesort.h"
+#include "../common/common.h"
+#include "../common/sync.h"
+
+int main(int argc, char** argv)
+{
+  long num_keys_log = DEFAULT_NUM_KEYS_LOG;
+  long num_keys = 1 << num_keys_log;
+  key_t* keys_in;
+  key_t* keys_out;
+  struct timeval start;
+  struct timeval end;
+  int option;
+  int check = 0;
+
+  while ((option = getopt(argc, argv, "n:s:hc")) != -1)
+    {
+      switch(option)
+	{
+	case 's':
+	  num_keys_log = atol(optarg);
+	  num_keys = 1 << num_keys_log;
+	  break;
+	case 'c':
+	  check = 1;
+	  break;
+	case 'h':
+	  printf("Usage: %s [option]...\n\n"
+		 "Options:\n"
+		 "  -s <power>                   Set the number of integer keys to 1 << <power>,\n"
+		 "  -c                           Perform check at the end\n"
+		 "                               default is %ld\n",
+		 argv[0], num_keys_log);
+	  exit(0);
+	  break;
+	case '?':
+	  fprintf(stderr, "Run %s -h for usage.\n", argv[0]);
+	  exit(1);
+	  break;
+	}
+    }
+
+  /* Allocate arrays for unsorted and sorted keys */
+  keys_in = malloc(num_keys*sizeof(key_t));
+  keys_out = malloc(num_keys*sizeof(key_t));
+
+  assert(keys_in);
+  assert(keys_out);
+
+  /* Fill keys_in with random keys */
+  init_sequence(keys_in, num_keys);
+
+  gettimeofday (&start, NULL);
+  PROFILER_NOTIFY_RECORD(&sync);
+  printf("Start sorting %ld keys...\n", num_keys);
+
+  /* Sort the array sequentially */
+  sort_block(keys_in, keys_out, num_keys);
+
+  printf("End sorting...\n");
+  PROFILER_NOTIFY_PAUSE(&sync);
+  gettimeofday (&end, NULL);
+
+  printf("%.5f\n", tdiff(&end, &start));
+
+  if(check) {
+    /* Check if keys are in ascending order */
+    for(long i = 0; i < num_keys; i++) {
+      if(i < num_keys-1)
+	assert(keys_out[i] <= keys_out[i+1]);
+    }
+
+    printf("Check: OK\n");
+  }
+
+  free(keys_in);
+  free(keys_out);
+
+  return 0;
+}
---
+++ examples/mergesort/mergesort.h
@@ -0,0 +1,93 @@
+/**
+ * Definitions and functions used both in the sequential and the
+ * parallel version of mergesort.
+ *
+ * Copyright (C) 2013 Andi Drebes <andi.drebes@lip6.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef MERGESORT_H
+#define MERGESORT_H
+
+#include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
+
+typedef uint64_t key_t;
+#define KEY_FORMAT "%"PRIu64
+
+#define DEFAULT_NUM_KEYS_LOG 20
+#define DEFAULT_BLOCK_SIZE_LOG 14
+
+/*
+ * Initializes an array of keys with random values
+ */
+static inline void init_sequence(key_t* keys, long num_keys)
+{
+  key_t rands = 77777;
+
+  for(long i = 0; i < num_keys; i++) {
+    rands = rands * 1103515245 + 12345;
+    keys[i] = rands;
+  }
+}
+
+/* Merges two sorted arrays keys_a and keys_b of equal length
+ * and writes the result to keys_out. Num_keys specifies the
+ * number of keys in keys_a and keys_b respectively.
+ */
+static inline void merge(key_t* keys_a, key_t* keys_b, key_t* out, long num_keys)
+{
+  long pos_a = 0;
+  long pos_b = 0;
+  long pos_out = 0;
+
+  while(pos_out < 2*num_keys) {
+    if(pos_a < num_keys && (pos_b >= num_keys || keys_a[pos_a] <= keys_b[pos_b]))
+      out[pos_out++] = keys_a[pos_a++];
+    else
+      out[pos_out++] = keys_b[pos_b++];
+  }
+}
+
+/* Performs a sequential merge sort on the keys in keys_in and
+ * writes the result to keys_out.
+ */
+static inline void sort_block(key_t* keys_in, key_t* keys_out, long num_keys)
+{
+  key_t* iki = keys_in;
+  key_t* iko = keys_out;
+  key_t* tmp;
+
+
+  for(long ibs = 1; ibs < num_keys; ibs = ibs << 1) {
+    for(long ib = 0; ib < num_keys / ibs; ib += 2) {
+      merge(&iki[ib*ibs], &iki[(ib+1)*ibs], &iko[ib*ibs], ibs);
+    }
+
+    tmp = iki;
+    iki = iko;
+    iko = tmp;
+  }
+
+  /* If the number of iterations of the main loop is even,
+   * the result is is the wrong array. Copy data to the
+   * output array.
+   */
+  if(iki == keys_in)
+    memcpy(keys_out, keys_in, num_keys*sizeof(key_t));
+}
+
+#endif
