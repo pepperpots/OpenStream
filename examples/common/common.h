83ea53bf225354f24ad6ba4db015ffbaca722102
Andi Drebes <andi.drebes@lip6.fr>
2013-06-19 15:57:22 +0200

    Use relative errors in double_equal

--- examples/common/common.h
+++ examples/common/common.h
@@ -11,7 +11,10 @@ double tdiff (struct timeval *end, struct timeval *start);
 static inline bool
 double_equal (double a, double b)
 {
-  return (fabs (a - b) < 1e-7);
+  if(a == 0.0)
+    return (fabs (a - b) < 1e-5);
+  else
+    return (fabs (a - b) / fabs(a) < 5e-5);
 }
 
 #endif
