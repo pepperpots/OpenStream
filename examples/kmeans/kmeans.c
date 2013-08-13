395abb2784cef8755d703dde9b28949f7ddcecd9
Andi Drebes <andi.drebes@lip6.fr>
2013-08-13 20:26:57 +0200

    Add serial version of k-means clustering

---
+++ examples/kmeans/kmeans.c
@@ -0,0 +1,386 @@
+/**
+ * Serial implementation of K-means, compatible to the non-fuzzy
+ * k-means version from minebench 3.0 without z-score transformation.
+ *
+ * Copyright (C) 2013 Andi Drebes <andi.drebes@lip6.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <float.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include "../common/common.h"
+#include "../common/sync.h"
+
+/**
+ * Returns a random float value in [-1.0;1.0]
+ */
+float frand(void)
+{
+	return (float)(rand() - RAND_MAX/2) / (float)RAND_MAX;
+}
+
+/**
+ * Returns the square value of the euclidian distance between
+ * two n-dimensional points ref and val
+ */
+static inline float euclid_dist_sq(int n, float* ref, float* val)
+{
+	float dist = 0.0f;
+	float curr;
+
+	for(int i = 0; i < n; i++) {
+		curr = val[i] - ref[i];
+		dist += curr * curr;
+	}
+
+	return dist;
+}
+
+/**
+ * Performns one single iteration of the k-means algorithm.
+ *
+ * k: number of clusters
+ * nd: number of dimensions
+ * ccenters: current cluster centers
+ * n: number of points
+ * vals: the points to be associated to the clusters
+ * membership: contains the ids of the clusters the points are currently
+ *             associated to. Contains the new cluster ids after the call
+ *
+ * Returns the fraction of points for which the id of the cluster changed.
+ */
+float kmeans(int k, int nd, float* ccenters, int n, float* vals, int* membership)
+{
+	float min_dist;
+	float curr_dist;
+	int min_cid = 0;
+	float delta = 0.0f;
+
+	/* Determine nearest cluster for each point */
+	for(int i = 0; i < n; i++) {
+		min_dist = FLT_MAX;
+
+		/* Calculate distance for each cluster center and
+		 * find id of the nearest cluster
+		 */
+		for(int cluster = 0; cluster < k; cluster++) {
+			curr_dist = euclid_dist_sq(nd, &ccenters[cluster*nd], &vals[i*nd]);
+
+			if(curr_dist < min_dist) {
+				min_dist = curr_dist;
+				min_cid = cluster;
+			}
+		}
+
+		/* Update membership */
+		if(membership[i] == -1 || membership[i] != min_cid) {
+			membership[i] = min_cid;
+			delta += 1.0f;
+		}
+	}
+
+	delta = delta / n;
+
+	return delta;
+}
+
+/**
+ * Initializes the membership array with -1
+ *
+ * n: number of points
+ * membership: array for memberships
+ */
+void init_membership(int n, int* membership)
+{
+	for(int i = 0; i < n; i++)
+		membership[i] = -1;
+}
+
+/**
+ * Initialize the cluster centers by choosing members
+ * from the set of points randomly.
+ *
+ * k: number of clusters
+ * nd: number of dimensions
+ * ccenters: array that contains the initial coordinates after the call
+ */
+void init_clusters(int k, int nd, float* ccenters, int n, float* vals)
+{
+	for(int i = 0; i < k; i++) {
+		/* Pick a random point */
+		int pt = rand() % n;
+
+		/* Copy the points' coordinates */
+		for(int dim = 0; dim < nd; dim++)
+			ccenters[i*nd+dim] = vals[pt*nd + dim];
+	}
+}
+
+/**
+ * Calculates the new cluster centers based on the new memberships
+ *
+ * k: number of clusters
+ * nd: number of dimensions
+ * ccenters: array that contains the updated coordinates after the call
+ * nmembers: stores how many members each cluster has
+ * n: number of points
+ * vals: the points
+ * membership: contains the ids of the clusters the points are currently
+ *             associated to
+ */
+void update_clusters(int k, int nd, float* ccenters, int* nmembers, int n, float* vals, int* membership)
+{
+	/* Reset cluster centers and number of members per cluster */
+	memset(ccenters, 0, sizeof(float)*k*nd);
+	memset(nmembers, 0, sizeof(int)*k);
+
+	/* Accumulate coordinates and count members */
+	for(int i = 0; i < n; i++) {
+		for(int dim = 0; dim < nd; dim++)
+			ccenters[membership[i] * nd + dim] += vals[i*nd + dim];
+
+		nmembers[membership[i]]++;
+	}
+
+	/* Normalize coordinates */
+	for(int clust = 0; clust < k; clust++)
+		for(int dim = 0; dim < nd; dim++)
+			ccenters[clust*nd + dim] /= nmembers[clust];
+}
+
+/**
+ * Read the contents of a file in minebench binary format.
+ *
+ * filename: full path to the file to be loaded
+ * nd: contains the number of dimensions after the call
+ * n: contains the number of points after the call
+ * vals: contains a pointer to the point data; the array
+ *       is allocated automatically.
+ *
+ * Returns 0 if the file could be read correctly, otherwise 1
+ */
+int read_binary_file(const char* filename, int* nd, int* n, float** vals)
+{
+	FILE* fp = fopen(filename, "rb");
+	int ret = 1;
+
+	if(!fp)
+		goto out;
+
+	/* Read number of points */
+	if(fread(n, sizeof(*n), 1, fp) != 1)
+		goto out_fp;
+
+	/* Read number of dimensions */
+	if(fread(nd, sizeof(*nd), 1, fp) != 1)
+		goto out_fp;
+
+	/* Allocate point array */
+	if(!(*vals = malloc((*n) * (*nd) * sizeof(float))))
+		goto out_fp;
+
+	/* Read point data */
+	if(fread(*vals, (*n) * (*nd) * sizeof(float), 1, fp) != 1) {
+		free(*vals);
+		goto out_fp;
+	}
+
+	ret = 0;
+
+out_fp:
+	fclose(fp);
+out:
+	return ret;
+}
+
+/**
+ * Initializes an array of points with random coordinates.
+ * For each cluster a random point is generated and n/k
+ * iterations of a random walk are performed.
+ *
+ * nd: number of dimensions
+ * n: number of points
+ * k: number of clusters
+ * vals: previously allocated array for point data
+ */
+void init_random_points_random_walk_clust(int nd, int n, int k, float* vals)
+{
+	int vals_per_clust = n / k;
+	float ccenter[nd];
+	float walk[nd];
+
+	for(int i = 0; i < n; i++) {
+		if(i % vals_per_clust == 0) {
+			memset(walk, 0, sizeof(walk));
+			for(int dim = 0; dim < nd; dim++)
+				ccenter[dim] = frand();
+		}
+
+		for(int dim = 0; dim < nd; dim++) {
+			walk[dim] += frand();
+			float val = walk[dim];
+			vals[i*nd+dim] = ccenter[dim] + val;
+		}
+	}
+}
+
+int main(int argc, char** argv)
+{
+	int k_min = 4;
+	int k_max = 13;
+	int nd = 3;
+	int n = 100000;
+	int niter = 500;
+	float threshold = 0.001;
+	float delta;
+	float* vals;
+	int option;
+	const char* in_file = NULL;
+	int* membership;
+	float* cluster_centers;
+	int* nmembers;
+	int i;
+	int verbose = 0;
+
+	struct profiler_sync sync;
+	struct timeval start;
+	struct timeval end;
+
+	/* Parse options */
+	while ((option = getopt(argc, argv, "d:i:m:n:p:t:vh")) != -1)
+	{
+		switch(option)
+		{
+			case 'd':
+				nd = atoi(optarg);
+				break;
+			case 'i':
+				in_file = optarg;
+				break;
+			case 'm':
+				k_max = atoi(optarg);
+				break;
+			case 'n':
+				k_min = atoi(optarg);
+				break;
+			case 'p':
+				n = atoi(optarg);
+				break;
+			case 'v':
+				verbose = 1;
+				break;
+			case 'h':
+				printf("Usage: %s [option]...\n\n"
+				       "Options:\n"
+				       "  -d <dimensions>              Number of dimensions; only required if no input\n"
+				       "                               file is specified; default is %d\n"
+				       "  -i <input file>              Read data from an input file in minebench binary format\n"
+				       "  -m <val>                     Maximal number of clusters, default is %d\n"
+				       "  -n <val>                     Minimal number of clusters, default is %d\n"
+				       "  -p <num_points>              Number of points; only required if no input\n"
+				       "                               file is specified; default is %d\n"
+				       "  -t <threshold>               Threshold for convergence, default is %f\n"
+				       "  -v                           Verbose output; disabled by default\n",
+				       argv[0], nd, k_max, k_min, n, threshold);
+				exit(0);
+				break;
+			case '?':
+				fprintf(stderr, "Run %s -h for usage.\n", argv[0]);
+				exit(1);
+				break;
+		}
+	}
+
+	if(in_file) {
+		if(read_binary_file(in_file, &nd, &n, &vals)) {
+			fprintf(stderr, "Could not read file %s.\n", argv[1]);
+			exit(1);
+		}
+	} else {
+		if(!(vals = malloc(n*nd*sizeof(float)))) {
+			fprintf(stderr, "Could not allocate point array.\n");
+			exit(1);
+		}
+
+		init_random_points_random_walk_clust(nd, n, k_max, vals);
+	}
+
+	if(!(membership = malloc(n*nd*sizeof(int)))) {
+		fprintf(stderr, "Could not allocate membership array.\n");
+		exit(1);
+	}
+
+	if(!(cluster_centers = malloc(k_max*nd*sizeof(float)))) {
+		fprintf(stderr, "Could not allocate cluster array.\n");
+		exit(1);
+	}
+
+	if(!(nmembers = malloc(k_max*nd*sizeof(int)))) {
+		fprintf(stderr, "Could not allocate array for the number of points per cluster.\n");
+		exit(1);
+	}
+
+	if(verbose) {
+		printf("-------- Start clustering -----------\n"
+		       "Dimensions: %d\n"
+		       "Points: %d\n"
+		       "Min. K: %d\n"
+		       "Max. K: %d\n"
+		       "Max. Iterations: %d\n\n",
+		       nd, n, k_min, k_max, niter);
+	}
+
+	PROFILER_NOTIFY_PREPARE(&sync);
+	gettimeofday(&start, NULL);
+	PROFILER_NOTIFY_RECORD(&sync);
+
+	/* Use the different number of clusters */
+	for(int k = k_min; k <= k_max; k++) {
+		/* Reset arrays and delta */
+		delta = threshold+1.0f;
+		init_membership(n, membership);
+		init_clusters(k, nd, cluster_centers, n, vals);
+
+		/* Perform at most niter iterations or break on convergence */
+		for(i = 0; i < niter && delta > threshold; i++) {
+			delta = kmeans(k, nd, cluster_centers, n, vals, membership);
+			update_clusters(k, nd, cluster_centers, nmembers, n, vals, membership);
+		}
+
+		if(verbose)
+			printf("K = %d, iterations = %d, final delta = %f\n", k, i-1, delta);
+	}
+
+	PROFILER_NOTIFY_PAUSE(&sync);
+	gettimeofday(&end, NULL);
+	PROFILER_NOTIFY_FINISH(&sync);
+
+	if(verbose)
+		printf("\n");
+
+	printf("%.5f\n", tdiff(&end, &start));
+
+	/* Cleanup arrays */
+	free(vals);
+	free(membership);
+	free(cluster_centers);
+	free(nmembers);
+
+	return 0;
+}
