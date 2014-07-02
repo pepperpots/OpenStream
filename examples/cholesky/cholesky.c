c9cb1536b8ed1fd21b74fde52ab336b039fc29b0
Andi Drebes <andi.drebes@lip6.fr>
2014-07-02 16:24:14 +0200

    Cholesky: Add padding to sequential version

--- examples/cholesky/cholesky.c
+++ examples/cholesky/cholesky.c
@@ -6,6 +6,7 @@
 #include <math.h>
 #include <getopt.h>
 #include "../common/common.h"
+#include "cholesky_common.h"
 
 #ifdef USE_MKL
   #include <mkl_cblas.h>
@@ -28,6 +29,9 @@ main(int argc, char *argv[])
 
   int N = 4096;
 
+  int padding = 64;
+  int padding_elements = padding / sizeof(double);
+
   int numiters = 10;
 
   FILE *res_file = NULL;
@@ -37,7 +41,7 @@ main(int argc, char *argv[])
   int nfo;
   char upper = 'U';
 
-  while ((option = getopt(argc, argv, "n:s:r:i:o:h")) != -1)
+  while ((option = getopt(argc, argv, "n:s:r:i:o:p:h")) != -1)
     {
       switch(option)
 	{
@@ -56,6 +60,17 @@ main(int argc, char *argv[])
 	case 'o':
 	  res_file = fopen(optarg, "w");
 	  break;
+	case 'p':
+	  padding = atoi(optarg);
+
+	  if(padding % sizeof(double)) {
+	    fprintf(stderr, "Padding must be a multiple of sizeof(double) (%lu).\n",
+		    sizeof(double));
+	    exit(1);
+	  }
+
+	  padding_elements = padding / sizeof(double);
+	  break;
 	case 'h':
 	  printf("Usage: %s [option]...\n\n"
 		 "Options:\n"
@@ -63,7 +78,9 @@ main(int argc, char *argv[])
 		 "  -s <power>                   Set the number of colums of the square matrix to 1 << <power>\n"
 		 "  -r <number of iterations>    Number of repetitions of the execution\n"
 		 "  -i <input file>              Read matrix data from an input file\n"
-		 "  -o <output file>             Write matrix data to an output file\n",
+		 "  -o <output file>             Write matrix data to an output file\n"
+		 "  -p <padding>                 Padding at the end of aline of the global matrix. Should be\n"
+		 "                               equal to the size of one cache line, e.g. 64.\n",
 		 argv[0], N);
 	  exit(0);
 	  break;
@@ -84,7 +101,7 @@ main(int argc, char *argv[])
   struct timeval *sstart = (struct timeval *) malloc (numiters * sizeof (struct timeval));
   struct timeval *send = (struct timeval *) malloc (numiters * sizeof (struct timeval));
 
-   if (posix_memalign ((void **) &data, 64, size * sizeof (double)))
+   if (posix_memalign ((void **) &data, 64, size * sizeof (double) + N * padding))
     {
       printf ("Out of memory.\n");
       exit (1);
@@ -110,19 +127,21 @@ main(int argc, char *argv[])
   for(i = 0; i < N; ++i)
     data[i*N + i] += N;
 
-
   for (iter = 0; iter < numiters; ++iter)
     {
       double * seq_data;
-      if (posix_memalign ((void **) &seq_data, 64, size * sizeof (double)))
+      if (posix_memalign ((void **) &seq_data, 64, size * sizeof (double) + N * padding))
 	{
 	  printf ("Out of memory.\n");
 	  exit (1);
 	}
       memcpy (seq_data, data, size * sizeof (double));
+      matrix_add_padding(seq_data, N, padding_elements);
+
+      int Npad = N+padding_elements;
 
       gettimeofday (&sstart[iter], NULL);
-      dpotrf_(&upper, &N, seq_data, &N, &nfo);
+      dpotrf_(&upper, &N, seq_data, &Npad, &nfo);
       gettimeofday (&send[iter], NULL);
 
       free (seq_data);
