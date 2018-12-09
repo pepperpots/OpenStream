bb2b8db016b3e2d3e908db0fba22f23b7705a195
Antoniu Pop <antoniu.pop@manchester.ac.uk>
2018-12-09 21:55:32 +0000

    Fix the random number generator.

--- libworkstream_df/prng.h
+++ libworkstream_df/prng.h
@@ -1,6 +1,8 @@
 #ifndef PRNG_H
 #define PRNG_H
 
+#include <limits.h>
+
 static inline void prng_init(unsigned int* rands, unsigned int worker_id)
 {
 	*rands = 77777 + worker_id * 19;
@@ -11,7 +13,7 @@ static inline unsigned int prng_nextb(unsigned int* rands, unsigned int mod_bits
 	*rands = (*rands) * 1103515245 + 12345;
 	unsigned int this_rands = (*rands);
 	unsigned int val = 0;
-	unsigned int mask = (1 << (mod_bits+1)) - 1;
+	unsigned int mask = mod_bits < sizeof(mask)*8 ? (1 << mod_bits) - 1 : UINT_MAX;
 	unsigned int total_bits = sizeof(val)*8;
 
 	for(unsigned int i = 0; i < total_bits; i += mod_bits) {
