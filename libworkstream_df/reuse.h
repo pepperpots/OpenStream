b21f5871df821e900b0d9a86f0caee050db8c0cf
Antoniu Pop <antoniu.pop@manchester.ac.uk>
2018-12-09 22:15:37 +0000

    Fix function declaration name for __built_in_wstream_df_reuse_prepare_data.

--- libworkstream_df/reuse.h
+++ libworkstream_df/reuse.h
@@ -1,3 +1,6 @@
+#ifndef REUSE_H
+#define REUSE_H
+
 #include "wstream_df.h"
 
 static inline int is_reuse_view(wstream_df_view_p v)
@@ -15,7 +18,7 @@ static inline int reuse_view_has_own_data(wstream_df_view_p v)
 	return !v->reuse_data_view;
 }
 
-void __built_in_wstream_df_reuse_prepare_data(void* v);
+void __built_in_wstream_df_prepare_data(void* v);
 void __built_in_wstream_df_reuse_update_data(void* v);
 void match_reuse_output_clause_with_reuse_input_clause(wstream_df_view_p out_view, wstream_df_view_p in_view);
 void match_reuse_input_clause_with_output_clause(wstream_df_view_p out_view, wstream_df_view_p in_view);
@@ -23,3 +26,4 @@ void match_reuse_output_clause_with_input_clause(wstream_df_view_p out_view, wst
 
 void check_reuse_copy(wstream_df_frame_p fp);
 
+#endif
