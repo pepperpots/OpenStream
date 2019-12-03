03d21ccd10901e980ecfa38b302c94226912f222
Igor Wodiany <igor.wodiany@postgrad.manchester.ac.uk>
2019-12-03 16:32:22 +0000

    libworkstream_df: Add FOS libraries to the makefile

---
+++ extras/setup-fos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Export required variables
+export LD_LIBRARY_PATH=~/Projects/OpenStream/extras/fos/install/lib
+export FOS_PATH=~/Projects/OpenStream/extras/fos
+
+# Clone the repository
+if [ -d "fos" ]
+then
+  cd fos
+  git pull
+  cd ..
+else
+  git clone https://github.com/khoapham/fos.git
+fi
+
+# Copy cmake files to fos tree
+cp -r fos-cmake/* fos
+
+# Build fos libraries
+cd fos
+mkdir -p build
+cd build
+/usr/bin/cmake -DCMAKE_BUILD_TYPE=Release ..
+make install
--- libworkstream_df/wstream_df.c
+++ libworkstream_df/wstream_df.c
@@ -1767,3 +1767,4 @@ void openstream_pause_hardware_counters(void)
 }
 
 }
+
